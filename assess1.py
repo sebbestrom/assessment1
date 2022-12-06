7dfc18d6ad797ccdd2a058b9d8a3f53773df23cc
sebbestrom <stroom42@gmail.com>
2022-12-06 11:27:51 +0100

    changes in py-file

--- assess1.py
+++ assess1.py
@@ -6,3 +6,23 @@ def connectdb():
         user="postgres",
         password="sudden21")
     return conn
+
+def list(conn):
+    conn = connectdb()
+    cur = conn.cursor()
+    cur.execute("SELECT*FROM contacts")
+    rows = cur.fetchall()
+    conn.commit()
+    cur.close()
+    return rows
+
+keep_going = True
+while keep_going:
+    commands = input("Following commands are available: list, insert, delete: ").strip().lower()
+    if commands == "list":
+        conn = connectdb()
+        print("\nThis is the list:")
+        listrows = list(conn)
+        for listrow in listrows:
+            print(listrow[0], "\t", listrow[1])
+        print("\n")
